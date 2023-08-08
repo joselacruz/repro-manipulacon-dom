f1c594e5d423ec243b97aa52ca8d6cbedb9a67e7
joselacruz <joselacruzperez@gmail.com>
2023-08-08 17:46:49 -0400

    loadin skeleton and lazy img

--- src/index.js
+++ src/index.js
@@ -3,50 +3,62 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 window.addEventListener("load", obtainProducts);
-const baseURL = 'https://platzi-avo.vercel.app/'
+const baseURL = "https://platzi-avo.vercel.app/";
 const todosLosItem = [];
+const containerAvo = document.querySelector(".container-avo");
 
 const formatPrice = (price) => {
-   const newPrice =  new window.Intl.NumberFormat('en-En', {
-        style: 'currency',
-        currency: 'USD'
-    }).format(price)
-return newPrice
-}
+  const newPrice = new window.Intl.NumberFormat("en-En", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+  return newPrice;
+};
 
-async function obtainProducts () {
-const response = await fetch(`${baseURL}api/avo`);
-const responseJson = await response.json();
+async function obtainProducts() {
+  try {
+    const response = await fetch(`${baseURL}api/avo`);
+    const responseJson = await response.json();
 
-responseJson.data.forEach(item => {
-    createProducts({
+    responseJson.data.forEach((item) => {
+      createProducts({
         src: `${baseURL}${item.image}`,
         name: item.name,
         precio: formatPrice(item.price),
-
+      });
     });
-    
-});
 
-const containerAvo = document.createElement('div');
-containerAvo.className = "container-avo";
-containerAvo.append(...todosLosItem);
-document.body.appendChild(containerAvo);
+    containerAvo.innerHTML = "";
+    containerAvo.append(...todosLosItem);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function createProducts ({src,name,precio }) {
- const image = document.createElement('img');
- image.src = src
- const title = document.createElement('h2');
- title.textContent = name;
- const price = document.createElement('p');
- price.textContent = `${precio}`
- const containerProduct  = document.createElement('div');
- containerProduct.append(image,title,price);
- 
- todosLosItem.push(containerProduct);
+function createProducts({ src, name, precio }) {
+  const image = document.createElement("img");
+  image.setAttribute("data-img", src);
+  const title = document.createElement("h2");
+  title.textContent = name;
+  const price = document.createElement("p");
+  price.textContent = `${precio}`;
+  const containerProduct = document.createElement("div");
+  containerProduct.append(image, title, price);
+
+  todosLosItem.push(containerProduct);
+  lazyLoader.observe(image);
 }
 
-// API INTL internacionalizacion 
+// API INTL internacionalizacion
 // 1- format fechas
 // 1- format monedas
+
+const lazyLoader = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const url = entry.target.getAttribute("data-img");
+      entry.target.setAttribute("src", url);
+      console.log(entry.target);
+    }
+  });
+});
